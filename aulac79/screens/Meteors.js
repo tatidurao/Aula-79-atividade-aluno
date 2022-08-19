7a24c4f0020a9021023e98d33913df4564826e0c
tatidurao <91564423+tatidurao@users.noreply.github.com>
2022-08-19 11:31:15 -0300

    Add files via upload

---
+++ aulac79/screens/Meteors.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    Alert,
+    FlatList,
+    Image,
+    ImageBackground,
+    Dimensions
+} from "react-native";
+import axios from "axios";
+
+export default class MeteorScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+           //criar estado meteors:{}
+        };
+    }
+
+    //criar função para chamada de api
+
+    
+
+    render() {
+        if () {
+           
+        } else {
+            let meteor_arr = Object.keys(this.state.meteors).map(meteor_date => {
+                return this.state.meteors[meteor_date]
+            })
+            let meteors = [].concat.apply([], meteor_arr);
+
+            meteors.forEach(function (element) {
+                let diameter = (element.estimated_diameter.kilometers.estimated_diameter_min + element.estimated_diameter.kilometers.estimated_diameter_max) / 2
+                let threatScore = (diameter / element.close_approach_data[0].miss_distance.kilometers) * 1000000000
+                element.threat_score = threatScore;
+            });
+            
+            
+            return (
+                
+            );
+        }
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height
+    },
+    titleBar: {
+        flex: 0.15,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    titleText: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "white"
+    },
+    meteorContainer: {
+        flex: 0.85
+    },
+    listContainer: {
+        backgroundColor: 'rgba(52, 52, 52, 0.5)',
+        justifyContent: "center",
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 5,
+        borderRadius: 10,
+        padding: 10
+    },
+    cardTitle: {
+        fontSize: 20,
+        marginBottom: 10,
+        fontWeight: "bold",
+        color: "white"
+    },
+    cardText: {
+        color: "white"
+    },
+    threatDetector: {
+        height: 10,
+        marginBottom: 10
+    },
+    gifContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1
+    },
+    meteorDataContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+
+    }
+});
